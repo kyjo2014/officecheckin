acec38a3add8d10bcbfe8f585654bb870813f707
kyjo2014 <467346087@qq.com>
2016-12-18 23:19:07 +0800

    query username

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express()
 
 const officeIP = 'localhost'
 
-let userList = []
+//当前用户列表
+let userList = {
+
+}
 //nginx has delete the x-forward-* head
 app.set('trust proxy', true)
 
@@ -13,21 +16,26 @@ app.get('/', (req, res) => {
     res.send('please register')
 })
 >
-app.get('/:username', (req, res) => {
+app.get('/', (req, res) => {
     var ipAddress = 1;
     var headers = req.headers;
-    var forwardedIpsStr = headers["x-real-ip"] 
+    var forwardedIpsStr = headers["x-real-ip"] || headers["x-forwarded-for"]
     forwardedIpsStr ? ipAddress = forwardedIpsStr : ipAddress = null;
     if (!ipAddress) {
         ipAddress = req.connection.remoteAddress;
     }
+
+    if (forwardedIpsStr == officeIP) { 
+
+    }
+
     let time = new Date()
 
     res.send({
         "yourIP": forwardedIpsStr,
         "headers": headers,
         "time": time.getTime(),
-        "username": req.params.username
+        "username": req.query.username
     })
     //1234567>8
     time.getTime()
